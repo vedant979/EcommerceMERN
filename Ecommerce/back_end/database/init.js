d89628412468409c4ad19febc95e501b2bb4078f
yogeshverma2 <yogesh.kumarverma@farmart.co>
2024-08-11 14:24:43 +0530

    done

--- Ecommerce/back_end/database/init.js
+++ Ecommerce/back_end/database/init.js
@@ -24,10 +24,10 @@ if (process.env.ISSQL) {
   };
 } else {
   module.exports = async function main() {
-    await mongoose.connect("mongodb://127.0.0.1:27017/EcommerceWithSeller");
-    // await mongoose.connect(
-    //  mongodconnect
-    // );
+    // await mongoose.connect("mongodb://127.0.0.1:27017/EcommerceWithSeller");
+    await mongoose.connect(
+     mongodconnect
+    );
 
     console.log("database connected");
     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
